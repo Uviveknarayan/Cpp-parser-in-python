3f28b083def231552918df674144f3877ba004da
vivek narayan uppala <u_vnarayan@cs.iitr.ac.in>
2024-03-26 11:38:29 +0530

    first commit

---
+++ test_programs/for.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t = 1;
+    for(int x = 0; x<10;x++){
+        cout<<"x: "<<x<<endl;
+    }
+    return 0;
+}
---
+++ test_programs/if.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t = 1;
+    while(t){
+       double d1 = 4.0;
+       cout<<"else"<<endl;
+    }
+    return 0;
+}
